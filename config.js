4a98624cdc962fc4d6b4d3b5f2fed94ff3d918e6
shashimal2000 <115067658+shashimal2000@users.noreply.github.com>
2024-04-11 10:16:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94788779949', 'shashimal', true],
+  ['94788779949', 'kavindu', true], 
   [''] 
 ] //Number of owners
 
